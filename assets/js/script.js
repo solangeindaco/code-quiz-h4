0b768ccf872227e814451b0268a9f550709c3e3c
SolangeI <solange.indaco@gmail.com>
2023-08-25 12:48:39 -0700

    Make an small change in the functionality of the highscores page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,22 @@
 // Access element by ID using .querySelector()
-let goBackBtn = document.querySelector("#go-back");
+let mainPresentation = document.querySelector("#main-presentation");
+let question = document.querySelector("#question");
+let startQuizBtn = document.querySelector("#start-quiz");
 
-goBackBtn.addEventListener("click", function(){
+startQuizBtn.addEventListener("click", function(){
+    //hide main presentation section
+    mainPresentation.setAttribute("display","none");
+    //show question section
+    question.setAttribute("display","block");
+});
+
+//This function is called when user click on the "goBack" button on the highscores page
+function goToMainPage(){
     // The location.href is change to navigate to the index page
     location.href = "./index.html";
-});
+}
+
+//This function is called when user click on the "clear Highscores" button on the highscores page
+function clearHighscores(){
+
+}
